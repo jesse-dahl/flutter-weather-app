4dcfee619bd8d36bbc774ce7b8791060dfac8537
jesse-dahl <jddahl199@gmail.com>
2019-07-07 21:28:05 -0600

    created a method that gets and returns a users latitude and longitude

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import '../services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -9,30 +8,24 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  Location location = Location();
   @override
   void initState() {
     super.initState();
-    location.getLocation();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
   }
 
   @override
   Widget build(BuildContext context) {
-    String myMargin = '15';
-    double myMarginAsDouble;
-
-    try {
-      myMarginAsDouble = double.parse(myMargin);
-    }
-    catch (e) {
-      print(e);
-    }
 
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(myMarginAsDouble ?? 15.0),
-        color: Colors.red,
-      ),
+
     );
   }
 }
--- lib/services/location.dart
+++ lib/services/location.dart
@@ -2,15 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
 class Location {
+
+  double latitude;
+  double longitude;
+
+  Location({this.latitude, this.longitude});
   
-  void getLocation() async {
+  Future<void> getCurrentLocation() async {
     try {
       Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-      print(position);
+      
+      latitude = position.latitude;
+      longitude = position.longitude;
+
     }
     catch(e) {
       print(e);
-      
     }
   }
 }

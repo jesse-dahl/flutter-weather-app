9b7d157c4e1eaa7edf2c0e00ce10e35dce48e9a8
jesse-dahl <jddahl199@gmail.com>
2019-07-08 18:17:04 -0600

    refactored some code

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -25,23 +25,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
-    if(response.statusCode == 200) {
-      String data = response.body;
-
-      var decodedData = jsonDecode(data);
-
-      var temperature = decodedData['main']['temp'];
-      var condition = decodedData['weather'][0]['id'];
-      var cityName = decodedData['name'];
-
-      print(temperature);
-      print(condition);
-      print(cityName);
-
-    } else {
-      print(response.statusCode);
-    }
+    
   }
 
   @override
